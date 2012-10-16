459c90747b052527ff968fa948758bfb590c9cb8
techmike2kx <mike@azug.be>
2012-10-16 05:40:50 +0200

    Azure Push 2

--- MyGHSite/MyGHSite/Controllers/HomeController.cs
+++ MyGHSite/MyGHSite/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyGHSite.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Home is no longer 127.0.0.1 :-) ";
+            ViewBag.Message = "Set Your text here.) ";
 
             return View();
         }
